ef9ce0ac9c7f222da1b517d34154c6a65bc8022e
nehaychaudhari44 <nehaychaudhari44@gmail.com>
2022-01-12 18:50:41 +0530

    service provider changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c1241bd6e5d9acc8f21cef6d309369f30c28b4a8',
+        'name' => 'indianic/country',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'indianic/country' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c1241bd6e5d9acc8f21cef6d309369f30c28b4a8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
