d35faa6eaec23f70e7676caad2e46c074c8e35be
nehaychaudhari44 <nehaychaudhari44@gmail.com>
2022-01-12 17:07:03 +0530

    provider update

--- src/Providers/CountryServiceProvider.php
+++ src/Providers/CountryServiceProvider.php
@@ -28,18 +28,11 @@ class CountryServiceProvider extends ServiceProvider
 
       $this->loadMigrationsFrom(__DIR__.'/../migrations','country');
 
-       $this->publishes([
-            __DIR__.'/assets' => public_path('vendor/country'),
-        ], 'public');
-
+       
         //config
         $this->publishes([
-            __DIR__.'/config/country.php' => config_path('country.php')
+            __DIR__.'/../../config/country.php' => config_path('country.php')
         ], 'config');
-
-        $this->publishes([
-            __DIR__.'/views' => resource_path('views/vendor/country')
-        ], 'views');
 
 	 }
 
