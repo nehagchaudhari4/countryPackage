d84225ba389fddea1188f019a0c7b2c08a366190
nehaychaudhari44 <nehaychaudhari44@gmail.com>
2022-01-12 22:38:35 +0530

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e71c24191eda7aed45ea09dcf38d26fc87c91405',
+        'name' => 'indianic/country',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'indianic/country' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e71c24191eda7aed45ea09dcf38d26fc87c91405',
+            'dev_requirement' => false,
+        ),
+    ),
+);
